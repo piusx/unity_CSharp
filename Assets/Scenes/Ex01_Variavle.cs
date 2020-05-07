02140e2d18bf28cc4eeb421a5884eed3d7154475
PiusX <45975396+piusx@users.noreply.github.com>
2020-05-07 13:15:42 +0900

    csharp ex01

---
+++ Assets/Scenes/Ex01_Variavle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex01_Variavle : MonoBehaviour
+{
+    // 1+x= 101
+    // 숫자 1- 상수
+    // x 변수
+    int x = 100; //변수 선언
+    int y = 100;
+    int sum; 
+
+    void Start()
+    {
+        //print(x); x 출력
+
+        sum = x + y;
+        print(sum);
+        print(-sum);
+    }
+
+    void Update()
+    {
+
+    }
+}
