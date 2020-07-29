c8945b92eb840ccab5f39fa97a2f08677ff35bfb
PiusX <45975396+piusx@users.noreply.github.com>
2020-07-30 02:26:12 +0900

    ep1~3

---
+++ Assets/Scripts/Ex02Test.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex02Test : MonoBehaviour
+{
+    int a = 100;
+    float b = 100.15f;
+    string cc;
+    int aa;
+    string dd = "100";
+
+    float sum2;
+    int sum;
+
+    // byte 정수 자료형 (0~255)
+    // sbyte 정수 자료형 ( -128 ~ +127) 1바이트
+    // short 정수 자료형 ( -3만 ~ +3만 )  - 2바이트
+    // integer 정수 자료형 ( -20억 ~ +20억 ) - 4바이트
+    // long 정수 자료형 ( 최대한 크기) - 8바이트
+
+    float f = 4.000001f;
+    double d = 4.00001;
+    decimal m = 4.000001m;
+
+    // float 실수 자료형 
+    // double 실수 자료형
+    // decimal 실수 자료형
+
+    string s = "문자";
+    char c = 'A';  // unicode 한글자만 기록
+
+    void Start()
+    {
+        sum = (int)(a + b); // 캐스트 강제 형변환. 소수점 절삭
+        sum2 = a + b;
+
+        print(sum2);
+        print(sum);
+
+
+        cc = a.ToString();
+        print(cc + "string");
+
+        aa = int.Parse(dd); // int로 강제 형변환
+        print(dd+"int");
+        
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ex03Test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex03Test : MonoBehaviour
+{
+    int intValue;
+
+    float floatValue = 10.5f;
+    float floatValue2 = 200.5f;
+
+
+    void FloatToInt(float _parameter, float _parameter2, string _stringParm) // 괄호안에 입력받을 함수
+    { // void 함수의 처리 결화에 반환값(int)이 없을때
+        intValue = (int)(_parameter + _parameter2); // 강제 형변환 
+        print(intValue);
+        print(_stringParm);
+    }
+
+    int FloatToInts(float _parameter, float _parameter2)
+    {
+        return (int)(_parameter + _parameter2);
+    }
+
+    void Start()
+    {
+        FloatToInt(floatValue, floatValue2, "테스트");
+        print(FloatToInts(floatValue, floatValue2));
+    }
+
+    void Update()
+    {
+        
+    }
+}
