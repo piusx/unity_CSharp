792bc5495fd8228d1d045ce6acb2478b6af7542a
PiusX <45975396+piusx@users.noreply.github.com>
2020-08-08 15:46:42 +0900

    go

---
+++ Assets/Scripts_Official/A01_VariablesAndFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A01_VariablesAndFunctions : MonoBehaviour
+{
+
+    int myInt = 10;
+
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt); // int 변수 적용
+        Debug.Log(myInt);
+    }
+
+    int MultiplyByTwo(int number) // int값을 반환
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+}
---
+++ Assets/Scripts_Official/A02_BasicSyntax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A02_BasicSyntax : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log(transform.position.x);
+
+        if(transform.position.y < 5f)
+        {
+            Debug.Log("I'm about to hit the ground!");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
