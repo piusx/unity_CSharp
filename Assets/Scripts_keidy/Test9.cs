ba4bfbdefd485c0dd4cd7a7d7fc1d8f1f181fcea
PiusX <45975396+piusx@users.noreply.github.com>
2021-04-09 21:30:39 +0900

    re

--- Assets/Scripts_keidy/Test9.cs
+++ Assets/Scripts_keidy/Test9.cs
@@ -4,47 +4,13 @@ using UnityEngine;
 
 public class Test9 : MonoBehaviour
 {
-    //배열
-    int[] exp = new int[5] { 1, 2, 3, 4, 5 };
+    int[] exp = { 50, 100, 200, 300, 500, 600, 700 };
 
-    // ArrayList
-    ArrayList arrayList = new ArrayList();
-
-    //List
-    List<int> list = new List<int>();
-
-    //HashTable
-    Hashtable hashTable = new Hashtable();
-
-    /*
     void Start()
     {
-        arrayList.Add(1);
-        arrayList.Add(2);
-        arrayList.Add(3);
-        arrayList.Add("가나다");
-        arrayList.Add(4.5);
-
-        //print(arrayList.Count);
-        //arrayList.Remove("가나다"); // 원하는 인덱스를 삭제
-        //arrayList.RemoveAt(3);  // 몇번째 인덱스를 삭제 
-        //arrayList.RemoveRange(1, 1);  //1부터 몇개를 지울까 결정
-        //arrayList.Clear(); // 초기화
-        //arrayList.Contains("가나다"); //있으면 트루
-
-        for (int i = 0; i < arrayList.Count; i++)
+        for (int i = 0; i <= exp.Length; i++)
         {
-            print(arrayList[i]);
+            print(exp[i]);
         }
     }
-    */
-
-    void Start()
-    {
-        hashTable.Add("만", 10000);
-        hashTable.Add("백만", 10000);
-
-        print(hashTable["만"]);
-    }
-
 }
